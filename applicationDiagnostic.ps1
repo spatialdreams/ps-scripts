09294229a8ec9c52a56fcc1ce603a5409d3e0de4
Jon <jnthnlmn@gmail.com>
2018-11-25 04:38:25 -0500

    Update applicationDiagnostic.ps1
    WIP, non functional

--- applicationDiagnostic.ps1
+++ applicationDiagnostic.ps1
@@ -3,20 +3,19 @@ $PSVersionTable.PSVersion
 #
 #parse command line arguments
 #
+
 param (
 [string]$desiredRuntime = "default"
   [Parameter(Mandatory=$true)][string]$delay,
   [string]$display = $("true")
 )
 
-$startTime = Get-Date -Uformat %T
-
-#info to aggregate:
-#ram usage, VRAM usage, cpu usage, cpu temp, gpu usage, gpu temp 
 #
+#data snapshot
+#
+
 function snapshot() {
-  $os = Get-Ciminstance Win32_OperatingSystem
-  
+  $os = Get-Ciminstance Win32_OperatingSystem 
   $key += Get-Date -Uformat %T  
   $vramUsage += 
   $memUsage += [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
@@ -26,6 +25,7 @@ function snapshot() {
 
 #program specific
 $day = Get-Date -Uformat %D 
+$startTime = Get-Date -Uformat %T
 $endTime = Get-Date -Uformat %T
 $elapsedTime = NEW-TIMESPAN -Start $startTime -End $endTime
 
@@ -36,71 +36,76 @@ $processor = Get-Ciminstance -ClassName win32_processor | format-list
 $ram = gwmi win32_physicalmemory
 
 
-
-#cpu info from ciminstance to gather
-#"Name": , "Availability": , "DeviceID": , "AddressWidth": , "DataWidth" , "CurrentClockSpeed": , "MaxClockSpeed": , "Family": , "LoadPercentage": , "UniqueId": , "Architecture": , "AssetTag": , "CpuStatus": "CurrentVoltage": , "ExtClock": , "L2CacheSize": , "L2CacheSpeed":, "L3CacheSize": , "L3CacheSpeed": , "Manufacturer": , "NumberOfCores": , "NumberOfEnabledCore": , "NumberOfLogicalProcessors": , "PartNumber", "ProcessorId": , "SocketDesignation": 
-
 #ram info to gather from RAM
 #"BankLabel":
 
 if ($desiredRunTime.Contains("H" -or "M" -or "S")) {
-	for { 
+  for { 
 	  
-	}
-	} elseif ($desiredRunTime.Contains("always") {
-	
-	
-#
-#header info
-#date, time: start time, end time, elapsed, record delay (seconds), system info: ram, cpu, os, 
-	$header = @"
-	"snapshotInfo":
-	  [
-	    {
-		  "date": $
-		  "endTime": $
-		  "startTime": $
-		  "elapsed": $
-		  "SystemInfo": 
-		    "cpu":
-			  [
-			    {
-			  
-			    }
-			  
-			  ]
-			"memory":
-			  [
-			    {
-				
-				}
-			  ]
-			"motherboard":
-			  [
-			    {
-				
-				}
-			  ]
-			"videocontroller":
-			  [
-			    {
-				
-				}
-			  ]
-		}
-	  ]
-	  "@
-	
-for scans in payload:	
-	payload":
-	[
-	  {
-	  
-	  }
-	]
-	} elseif ($desitredRunTime.Contains("default") {
-	
-	} else {
-	
+  }
+} elseif ($desiredRunTime.Contains("always") {
+
 }
 
+	
+#
+# Object Structure
+#
+
+$header = @{
+  ProgramInfo = @{
+    date = $day,
+    endTime = $endTime
+    startTime = $startTime
+    elapsed = $elapsedTime
+    }
+  cpu: @{
+    Name = $,
+    Manufacturer = $,
+    SocketDesignation = $,
+    CurrentClockSpeed = $,
+    MaxClockSpeed = $,
+    NumberOfCores = $, 
+    NumberOfEnabledCore = $, 
+    NumberOfLogicalProcessors = $,
+    L2CacheSize = $, 
+    L2CacheSpeed = $, 
+    L3CacheSize = $, 
+    L3CacheSpeed = $, 
+    Architecture = $,
+    Family = $,
+    ExtClock = $,  
+    DeviceID = $,
+    UniqueId = $
+    }
+  memory: @{
+    Brand = $,
+    modules = $,
+    totalMemory = $,
+     = $,
+     = $
+    }
+  motherboard: @{
+    Brand = $,
+     = $,
+     = $,
+     = $
+    }
+  videocontroller: @{
+     = $,
+     = $,
+     = $,
+     = $
+    }
+}
+	
+Payload @{	 
+  [scan$i]@{
+    ramUsage = $,
+	cpuUsage = $,
+	cpuTemp = $,
+	gpuUsage = $,
+	gpuTemp = $,
+	 = $
+  }
+}
