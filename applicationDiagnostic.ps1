6f5a2218a544a7e8f2c7b8f7f8d513d8676f3bc2
Jon <jnthnlmn@gmail.com>
2018-11-25 01:41:02 -0500

    Update applicationDiagnostic.ps1
    WIP, non functioning

--- applicationDiagnostic.ps1
+++ applicationDiagnostic.ps1
@@ -1,45 +1,106 @@
+$PSVersionTable.PSVersion
+
 #
 #parse command line arguments
 #
 param (
-[string]$desiredRuntime = "2m"
+[string]$desiredRuntime = "default"
   [Parameter(Mandatory=$true)][string]$delay,
   [string]$display = $("true")
 )
-
-switch ($desiredRunTime) {
-	"H":"M":"S" {
-	
-	}
-	program {
-	
-	}
-	always {
-	}
-	
-}
 
 $startTime = Get-Date -Uformat %T
 
 #info to aggregate:
 #ram usage, VRAM usage, cpu usage, cpu temp, gpu usage, gpu temp 
 #
-
-while 
+function snapshot() {
+  $os = Get-Ciminstance Win32_OperatingSystem
+  
+  $key += Get-Date -Uformat %T  
+  $vramUsage += 
+  $memUsage += [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
+  $cpuLoad +=
 }
-$key = Get-Date -Uformat %T
-$os = Get-Ciminstance Win32_OperatingSystem
-$vramUsage += 
-$memUsage = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
-sleep($delay)
-}
 
-#header info
-#date, time: start time, end time, elapsed, record delay (seconds), system info: ram, cpu, os, 
+
+#program specific
 $day = Get-Date -Uformat %D 
 $endTime = Get-Date -Uformat %T
 $elapsedTime = NEW-TIMESPAN -Start $startTime -End $endTime
-$videoController = wmic path win32_VideoController get name 
-$motherboard = gwmi Win32_BaseBoard
-$processor = Get-Ciminstance -ClassName Win32_Processor
+
+#user specific
+$motherboard = gwmi win32_baseboard
+$videoController = wmic path win32_videocontroller get name 
+$processor = Get-Ciminstance -ClassName win32_processor | format-list
 $ram = gwmi win32_physicalmemory
+
+
+
+#cpu info from ciminstance to gather
+#"Name": , "Availability": , "DeviceID": , "AddressWidth": , "DataWidth" , "CurrentClockSpeed": , "MaxClockSpeed": , "Family": , "LoadPercentage": , "UniqueId": , "Architecture": , "AssetTag": , "CpuStatus": "CurrentVoltage": , "ExtClock": , "L2CacheSize": , "L2CacheSpeed":, "L3CacheSize": , "L3CacheSpeed": , "Manufacturer": , "NumberOfCores": , "NumberOfEnabledCore": , "NumberOfLogicalProcessors": , "PartNumber", "ProcessorId": , "SocketDesignation": 
+
+#ram info to gather from RAM
+#"BankLabel":
+
+if ($desiredRunTime.Contains("H" -or "M" -or "S")) {
+	for { 
+	  
+	}
+	} elseif ($desiredRunTime.Contains("always") {
+	
+	
+#
+#header info
+#date, time: start time, end time, elapsed, record delay (seconds), system info: ram, cpu, os, 
+	$header = @"
+	"snapshotInfo":
+	  [
+	    {
+		  "date": $
+		  "endTime": $
+		  "startTime": $
+		  "elapsed": $
+		  "SystemInfo": 
+		    "cpu":
+			  [
+			    {
+			  
+			    }
+			  
+			  ]
+			"memory":
+			  [
+			    {
+				
+				}
+			  ]
+			"motherboard":
+			  [
+			    {
+				
+				}
+			  ]
+			"videocontroller":
+			  [
+			    {
+				
+				}
+			  ]
+		}
+	  ]
+	  "@
+	
+for scans in payload:	
+	payload":
+	[
+	  {
+	  
+	  }
+	]
+	} elseif ($desitredRunTime.Contains("default") {
+	
+	} else {
+	
+}
+
