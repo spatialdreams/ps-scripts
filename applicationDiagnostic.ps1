05c8d7dbe3a8ba25027a6a3b6ecb5564572ee15c
Jon <jnthnlmn@gmail.com>
2018-11-26 19:25:43 -0500

    Update applicationDiagnostic.ps1
    WIP, nonfunctioning

--- applicationDiagnostic.ps1
+++ applicationDiagnostic.ps1
@@ -1,14 +1,13 @@
-#not really imports, but they are imports
-$PSVersionTable.PSVersion
+#get system info
+$PSVersionTable.PSVersiong
 $mb = gwmi win32_baseboard
-$vc = wmic path win32_videocontroller get name 
-$cpu = Get-Ciminstance -ClassName win32_processor 
+$vc = gwmi win32_videocontroller 
 $ram = gwmi win32_physicalmemory
+$cpu = gwmi win32_processor 
 
 #
 #parse command line arguments
 #
-
 param (
   [string]$runtime = "default",
   [Parameter(Mandatory=$true)][Int32]$delay,
@@ -16,77 +15,103 @@ param (
 )
 
 if ($runtime.Contains("H" -or "M" -or "S")) {
-  for (i in len($runtime)){ 
-    
+  for (i$ in len($runtime)){ 
+    if ($i = H) {
+      $hours = $i.split({"H"})	  
+	}
+    if ($i = M) {
+      $hours = $i.split({"M"})
+
+	if ($i = S) {
+	}
+      $seconds = $i.split({"S"})
+	}
   }
 } elseif ($runtime.Contains("program") {
 
+} else {
+   
 }
 
-	
+#
+# program outline
+#
+
+$Payload = @()
+$startTime = Get-Date -Uformat %T 
+for loop {
+$Payload.append(snapshot())
+}
+$endTime = Get-Date -Uformat %T
+$elapsedTime = NEW-TIMESPAN -Start $startTime -End $endTime
+
 #
 # Object Structure
 #
 
-$startTime = Get-Date -Uformat %T
-
-Payload @{	 
-  [scan$i]@{
-  
-    ramUsage = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2),
-	vramUsage = $,
+function snapshot {
+  $livecpu = Get-Counter '\Memory\Available MBytes'
+  $liveram = Get-Counter '\Processor(_Total)\% Processor Time'
+  scan = @(
+    ramUsage = [math]::Round(( / )*100,2),
+    vramUsage = $,
     cpuUsage = $,
     cpuTemp = $,
     gpuUsage = $,
-    gpuTemp = $
-  }
+    gpuTemp = $,
+    procList = 
+  )
 }
-
-$endTime = Get-Date -Uformat %T
-$elapsedTime = NEW-TIMESPAN -Start $startTime -End $endTime
-
 $header = @{
   ProgramInfo = @{
     date = $day,
     endTime = $endTime
     startTime = $startTime
     elapsedTime = $elapsedTime
-    }
-  cpu: @{
+  }
+  motherboard = @{
+    Manufacturer = $mb.manufacturer,
+    SerialNumber = $mb.serialnumber,
+    ProductCode = $mb.product
+  }
+  cpu= @{
     Name = $cpu.name,
     Manufacturer = $cpu.manufacturer,
-    SocketDesignation = $cpu.socketdesignation,
-    CurrentClockSpeed = $cpu.currentclockspeed,
-    MaxClockSpeed = $cpu.maxclockspeed,
-    NumberOfCores = $cpu.numberofcores, 
-    NumberOfEnabledCore = $cpu.numberofenabledcore, 
-    NumberOfLogicalProcessors = $cpu.numberoflogicalprocessors,
+    SocketDesignation = $cpu.SocketDesignation,
+    CurrentClockSpeed = $cpu.CurrentClockSpeed,
+    MaxClockSpeed = $cpu.MaxClockSpeed,
+    NumberOfCores = $cpu.NumberOfCores, 
+    NumberOfEnabledCore = $cpu.NumberOfCores, 
+    NumberOfLogicalProcessors = $cpu.NumberOfLogicalProcessors,
     L2CacheSize = $cpu.l2cachesize, 
     L2CacheSpeed = $cpu.L2CacheSpeed, 
     L3CacheSize = $cpu.L3CacheSize, 
     L3CacheSpeed = $cpu.L3CacheSpeed, 
     Architecture = $cpu.Architecture,
     Family = $cpu.Family,
-    ExtClock = $cpu.extfamily,  
+    ExtClock = $cpu.extfamily,
     DeviceID = $cpu.deviceid,
     UniqueId = $cpu.uniqueid
-    }
-  memory: @{
-    Manufacturer = $,
-    modules = $,
-    TotalMemory = $,
-     = $,
-     = $
-    }
-  motherboard: @{
-    Manufacturer = $mb.manufacturer,
-    SerialNumber = $mb.serialnumber,
-    ProductCode = $mb.prodcut
-    }
-  videocontroller: @{
-     Manufacturer= $,
-     = $,
-     = $,
-     = $
-    }
+  }
+  memory = @{
+    TotalMemory = $ram.,
+    Modules = @(
+      module = @{
+        
+        Manufacturer = $,
+         = $,
+         = $
+      }
+    )
+  }
+
+  videocontroller = @{
+     Name = $vc.name,
+     Manufacturer = $vc.manufacturer,
+     VideoMemory = $vc.adapterram,
+     MemoryType = $vc.videomemorytype
+  }
+  operatingSystem = @{
+  
+  }
 }
