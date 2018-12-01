40549c895abc9b3cf13b9134631992893f6a2934
Jon <jnthnlmn@gmail.com>
2018-12-01 03:41:35 -0500

    Update applicationDiagnostic.ps1
    WIP

--- applicationDiagnostic.ps1
+++ applicationDiagnostic.ps1
@@ -27,72 +27,71 @@ param (
 #
 $header = @{
   ProgramInfo = @{
-    date = Get-Date -Uformat %D,
-    endTime = $endTime
-    startTime = $startTime
-    elapsedTime = $elapsedTime
-	psversion = $PSVersionTable.PSVersion
+    date = Get-Date -Uformat %D;
+    endTime = $endTime;
+    startTime = $startTime;
+    elapsedTime = $elapsedTime;
+	psversion = $PSVersionTable.PSVersion;
   }
   motherboard = @{
-    Manufacturer = $mb.manufacturer,
-    SerialNumber = $mb.serialnumber,
-    ProductCode = $mb.product
+    Manufacturer = $mb.manufacturer;
+    SerialNumber = $mb.serialnumber;
+    ProductCode = $mb.product;
   }
   cpu = @{
-    Name = $cpu.name,
-    Manufacturer = $cpu.manufacturer,
-    SocketDesignation = $cpu.SocketDesignation,
-    CurrentClockSpeed = $cpu.CurrentClockSpeed,
-    MaxClockSpeed = $cpu.MaxClockSpeed,
-    NumberOfCores = $cpu.NumberOfCores, 
-    NumberOfEnabledCore = $cpu.NumberOfCores, 
-    NumberOfLogicalProcessors = $cpu.NumberOfLogicalProcessors,
-    L2CacheSize = $cpu.l2cachesize, 
-    L2CacheSpeed = $cpu.L2CacheSpeed, 
-    L3CacheSize = $cpu.L3CacheSize, 
-    L3CacheSpeed = $cpu.L3CacheSpeed, 
-    Architecture = $cpu.Architecture,
-    Family = $cpu.Family,
-    ExtClock = $cpu.extfamily,
-    DeviceID = $cpu.deviceid,
-    UniqueId = $cpu.uniqueid
+    Name = $cpu.name;
+    Manufacturer = $cpu.manufacturer;
+    SocketDesignation = $cpu.SocketDesignation;
+    CurrentClockSpeed = $cpu.CurrentClockSpeed;
+    MaxClockSpeed = $cpu.MaxClockSpeed;
+    NumberOfCores = $cpu.NumberOfCores; 
+    NumberOfEnabledCore = $cpu.NumberOfCores; 
+    NumberOfLogicalProcessors = $cpu.NumberOfLogicalProcessors;
+    L2CacheSize = $cpu.l2cachesize; 
+    L2CacheSpeed = $cpu.L2CacheSpeed; 
+    L3CacheSize = $cpu.L3CacheSize; 
+    L3CacheSpeed = $cpu.L3CacheSpeed; 
+    Architecture = $cpu.Architecture;
+    Family = $cpu.Family;
+    ExtClock = $cpu.extfamily;
+    DeviceID = $cpu.deviceid;
+    UniqueId = $cpu.uniqueid;
   }
   memory = @{
     TotalMemory = $ram | Measure-Object -Property capacity -Sum | select sum
-	bank = @()
-	foreach ($module in $ram){
-        bank.append(
-          Manufacturer = $module.manufacturer,
-          Speed = $module.speed,
-          Capacity = $module.Capacity,
-		  PartNumber = $module.PartNumber,
-		  SerialNumber = $module.SerialNumber,
-		  tag = $module.tag
-       )
-	}
+	bank = @( 
+      foreach ($module in $ram){
+      $ = @{
+        Manufacturer = $module.manufacturer;
+        Speed = $module.speed;
+        Capacity = $module.Capacity;
+        PartNumber = $module.PartNumber;
+        SerialNumber = $module.SerialNumber;  
+      }
+    )
   }
 
-  $videocontroller = @{
-     Name = $vc.name,
-     Manufacturer = $vc.manufacturer,
-     VideoMemory = $vc.adapterram,
-     MemoryType = $vc.videomemorytype,
-	 InstalledDisplayDrivers = $InstalledDisplayDrivers
+  videocontroller = @{
+     Name = $vc.name;
+     Manufacturer = $vc.manufacturer;
+     VideoMemory = $vc.adapterram;
+     MemoryType = $vc.videomemorytype;
+	 InstalledDisplayDrivers = $InstalledDisplayDrivers;
   }
-  $operatingSystem = @{
-    SystemDirectory  = $os.SystemDirectory,
-    Organization = $os.Organization,
-    BuildNumber = $os.BuildNumber,
-    RegisteredUser = $os.RegisteredUser,
-    SerialNumber = $os.SerialNumber,
+  operatingSystem = @{
+    SystemDirectory  = $os.SystemDirectory;
+    Organization = $os.Organization;
+    BuildNumber = $os.BuildNumber;
+    RegisteredUser = $os.RegisteredUser;
+    SerialNumber = $os.SerialNumber;
     Version = $os.Version
   }
-  $bios = @{
-    Name = $bios.Name,
-    Version = $bios.Version,
-    SMBIOSBIOSVersion = $bios.SMBIOSBIOSVersion,
-    Manufacturer = $bios.Manufacturer,
-    SerialNumber = $bios.SerialNumber
+  bios = @{
+    Name = $bios.Name;
+    Version = $bios.Version;
+    SMBIOSBIOSVersion = $bios.SMBIOSBIOSVersion;
+    Manufacturer = $bios.Manufacturer;
+    SerialNumber = $bios.SerialNumber;
   }
 }
 
@@ -100,13 +99,13 @@ function snapshot() {
   $liveram = Get-Counter '\Memory\Available Bytes'
   $livecpu = Get-Counter '\Processor(_Total)\% Processor Time'
   $scan = @(
-    ramUsage = [math]::Round( 100 - ( $liveram.countersamples.cookedvalue / $header.totalmemory.sum) * 100,2 ),
-    cpuUsage = [math]::Round( $livecpu.countersamples.cookedvalue ),
-	#vramUsage = $,
-    #cpuTemp = $,
-    #gpuUsage = $,
-    #gpuTemp = $,
-    procList = get-process
+    ramUsage = [math]::Round( 100 - ( $liveram.countersamples.cookedvalue / $header.totalmemory.sum) * 100;2 );
+    cpuUsage = [math]::Round( $livecpu.countersamples.cookedvalue );
+    #vramUsage = $;
+    #cpuTemp = $;
+    #gpuUsage = $;
+    #gpuTemp = $;
+    procList = get-process;
   )
   return scan
 }
